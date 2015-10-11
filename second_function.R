47a7674945e0b23ec87031293a83fb6798380b07
rfzu <rall@list.ru>
2015-10-11 17:35:18 +0300

    second function lesson

---
+++ second_function.R
@@ -0,0 +1,36 @@
+f <- function(a, b = 1, c = 2, d = NULL) {
+
+}
+
+f <- function(a, b) {
+    print(a)
+    print(b)
+}
+
+myplot <- function(x,y,type = 'l', ...) {
+    plot(x,y,type = type, ...)  
+}
+
+# metaprogramming stuff!
+
+make.power <- function(n) {
+    pow <- function(x) {
+        x^n
+    }
+}
+
+cube <- make.power(3)
+make.power(2) -> square
+
+# lexical scoping - is a strange thing
+
+y <- 10
+
+f <- function(x) {
+    y <- 2
+    y^2 + g(x)
+}
+
+g <- function(x) {
+    x*y
+}

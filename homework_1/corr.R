f93f64eb806d5d8e824e78c7410804bbe67d8b43
rfzu <rall@list.ru>
2015-10-11 23:38:47 +0300

    wip

--- homework_1/corr.R
+++ homework_1/corr.R
@@ -10,5 +10,22 @@ corr <- function(directory, threshold = 0) {
         ## Return a numeric vector of correlations
         ## NOTE: Do not round the result!
 
-        
+        # id   = 1:332
+        # path = paste('~/Documents/code/R/studyR/homework_1/',directory,'/', formatC(id,width=3,flag='0'),'.csv', sep='')        
+
+        compl <- complete(directory)
+        id <- compl[,1][which(compl$nobs > threshold)]
+        # print(id)
+        path = paste('~/Documents/code/R/studyR/homework_1/',directory,'/', formatC(id,width=3,flag='0'),'.csv', sep='')
+
+        data = c()
+        for(p in path) {
+                conn =  file(p,'r')
+                data <- rbind(data, read.csv(conn))
+                close(conn)
+        }
+
+        # print(data)
+        cc = complete.cases(data$sulfate & data$nitrate)
+        cor(data$sulfate[cc], data$nitrate[cc])
 }
